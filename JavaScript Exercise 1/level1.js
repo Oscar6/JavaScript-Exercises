eeea61a8c94b993c2c2da70800df1ff68dc5cfb1
Oscar6 <oscar.m1024@gmail.com>
2018-12-04 23:00:09 -0600

    updated

---
+++ JavaScript Exercise 1/level1.js
@@ -0,0 +1,51 @@
+// var a = "hello"
+// function lvl1exercise1() {
+// 	// Declare a variable without instantiating it and return it!
+// }
+// console.log(a)
+
+// var number = 2;
+// function lvl1exercise2() {
+// 	// Declare and instantiate a number and return it
+// }
+// console.log(number)
+
+// var i
+// function lvl1exercise3() {
+	// Declare and instantiate a floating point number that isn't a whole number and return i
+// 	i = 5.4
+// 	console.log(i)
+// }
+// console.log(i)
+// lvl1exercise3()
+
+// var str = "Hello World!"
+// function lvl1exercise4() {
+	// Declare and instantiate a string "Hello World!" and return it
+// }
+// console.log(str)
+
+// var arr = ["Hello World!", 4];
+// function lvl1exercise5() {
+// 	 Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+// }
+// console.log(arr)
+
+// var objKey
+// function lvl1exercise6() {
+// 	 Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+// 	var objKey = {
+// 		key1: "Hello World!",
+// 		key2: 4
+// 	}
+// 	console.log(objKey)
+// }
+
+// console.log(objKey)
+// lvl1exercise6()
+
+// var sad = !sad;
+// function lvl1exercise7() {
+// 	// Declare and instantiate a boolean value 'false' and return it
+// }
+// console.log(sad)
---
+++ JavaScript Exercise 1/level2.js
@@ -0,0 +1,39 @@
+
+function lvl2exercise1(num1, num2) {
+	// Return the sum of num1 and num2
+	var sum = num1 + num2
+	return(sum)
+}
+console.log(lvl2exercise1(3, 5))
+
+
+function lvl2exercise2(num1, num2) {
+	// Return the difference of num1 and num2
+	var difference = num1 - num2
+	return(difference)
+}
+console.log(lvl2exercise2(3, 4))
+
+
+function lvl2exercise3(num1, num2) {
+	// Return the multiplication of num1 and num 2
+	var product = num1 * num2
+	return(product)
+}
+console.log(lvl2exercise3(3, 4))
+
+
+function lvl2exercise4(num1, num2) {
+	// Return the division of num1 and num2
+	var quotient = num1 / num2
+	return(quotient)
+}
+console.log(lvl2exercise4(12, 6))
+
+
+function lvl2exercise5(num1) {
+	// Add 2 to num1 using += and return it
+	var sum = num1 += 2
+	return(sum)
+}
+console.log(lvl2exercise5(12))
