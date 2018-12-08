b06d6537bd9c7136f6e00694d192f986eb9e0a5d
Oscar6 <oscar.m1024@gmail.com>
2018-12-07 22:14:43 -0600

    complete challenges

--- JavaScript Exercise 1/algorithmFriday.js
+++ JavaScript Exercise 1/algorithmFriday.js
@@ -1,12 +1,63 @@
 // Challenge 1
 
-var num
-function collatz() {
-    while num%2 == 0
-        return num/2;
-
-    else (num%2 != 0) 
-        return num * 3 + 1;
+function collatz(num) {
+    while (num != 1) {
+      if (num % 2 == 0) {
+        num = num / 2;
+        console.log(num)
+      } 
+      else {
+        num = num * 3 + 1;
+      console.log(num)
+      }
+    }
 }
+collatz(10)
 
-console.log(num);
+// Challenge 2
+// function largestPalendrome() {
+//    for (var i = 999; i > 899; i--) {
+//        for (var j = i; j > 899; j--) {
+//            let num = i * j;
+//            if (isPal(num)){
+//                return(num);
+//            }
+//        }
+//    }
+// }
+
+// console.log(largestPalendrome());
+
+// function isPal(num) {
+//    palStr = num.toString().split("");
+//    for (var i = 0; i < palStr.length / 2; i++) {
+//        if (palStr[i] != palStr[palStr.length - (1 + i)]) {
+//            return false;
+//        }
+//    }
+//    return true;
+// }
+
+// Output should be 906609
+
+// Challenge 3
+// function smallest(){
+//    var num = 2000;
+//    while (!isDivisible(num)){
+//        num++;
+//    }
+//    console.log(num);
+// }
+
+// function isDivisible(num) {
+//    for (var i = 1; i < 21; i++) {
+//        if (num % i != 0) {
+//            return false;
+//        }
+//    }
+//    return true;
+// }
+
+// smallest();
+
+// Output should be 232792560
