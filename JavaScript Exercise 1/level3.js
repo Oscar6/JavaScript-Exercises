f2f44ea90cd685c0a435091c0c01b28e83993b33
Oscar6 <oscar.m1024@gmail.com>
2018-12-09 22:09:56 -0600

    levels 1-6 of 7 complete

--- JavaScript Exercise 1/level3.js
+++ JavaScript Exercise 1/level3.js
@@ -1,25 +1,37 @@
 function lvl3exercise1() {
 	// Create a "hello" and a "world", return the concatenation of the two
-
+	var str1 = "Hello ";
+	var str2 = "world!";
+	return (str1 + str2)
 }
+console.log(lvl3exercise1());
 
 function lvl3exercise2() {
 	// Create a "hello" and a 12, return the concatenation of the two
-
+	var str1 = "Hello ";
+	var i = 12;
+	return (str1 + i)
 }
+console.log(lvl3exercise2());
 
 function lvl3exercise3() {
 	// Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+	var i = 12;
+	return (i.toString());
 }
+console.log(lvl3exercise3());
 
 function lvl3exercise4() {
 	// Create a "hello world!" string. Return the length of the string
-
+	var str = "hello world!"
+	return (str.length)
 }
+console.log(lvl3exercise4());
 
 function lvl3exercise5() {
 	// Create a "hello world!" string. Return the index of the word "world".
-	
+	var str = "hello world!"
+	return (str.indexOf("world"))
 }
+console.log(lvl3exercise5());
 
--- JavaScript Exercise 1/level4.js
+++ JavaScript Exercise 1/level4.js
@@ -1,30 +1,45 @@
 function lvl4exercise1() {
 	// Return the boolean value "true"
-	
+	var tOf = true;
+	return (tOf)
 }
+console.log(lvl4exercise1());
 
 function lvl4exercise2() {
 	// Return the boolean value "false"
-
+	var tOf = !true;
+	return (tOf)
 }
+console.log(lvl4exercise2());
 
 function lvl4exercise3(bool) {
 	// Return the opposite of the input boolean value
-
+	var bool = true;
+	return (!bool)
 }
+console.log(lvl4exercise3());
 
 function lvl4exercise4(bool1, bool2) {
 	// Return the logical "and" of the input boolean values
-
+	var bool1 = true;
+	var bool2 = false;
+	return (bool1 && bool2)
 }
+console.log(lvl4exercise4());
 
 function lvl4exercise5(bool1, bool2) {
 	// Return the logical "or" of the input boolean values
-	
+	var bool1 = true;
+	var bool2 = false;
+	return (bool1 || bool2)
 }
+console.log(lvl4exercise5());
 
 function lvl4exercise6(bool1, bool2) {
 	// Return the logical "equivalence" of the input boolean values
-	
+	var bool1 = 3;
+	var bool2 = 3;
+	return (bool1 == bool2)
 }
+console.log(lvl4exercise6());
 
